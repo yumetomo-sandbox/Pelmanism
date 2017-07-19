db22c170cf314f46a2d1591611d7889e548827d5
yumetomo <yumetomo.eto@imjp.co.jp>
2017-07-19 15:35:06 +0900

    file add

---
+++ app/webroot/js/index.bundle.js
@@ -0,0 +1,17 @@
+webpackJsonp([0],{
+
+/***/ 3:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var $ = __webpack_require__(0);
+
+$(function () {
+  //alert("ok");
+});
+
+/***/ })
+
+},[3]);
