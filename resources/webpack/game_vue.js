af718f5829484f7f8e05fe5347577dcd4426d80c
yumetomo <yumetomo.eto@imjp.co.jp>
2018-04-23 15:20:18 +0900

    [add] vueで一通りゲームの流れ作成

--- resources/webpack/game_vue.js
+++ resources/webpack/game_vue.js
@@ -1,6 +1,175 @@
+import _ from 'underscore';
+import CONFIG from './modules/Config';
+
 new Vue({
   el: '#app',
   data: {
-    message: 'hello vue'
+    count: CONFIG.COUNT,
+    cards: [],
+    colors: [],
+    openCards: [],
+    matchCount: 0,
+    totalCard: CONFIG.COLORS.length * 2
+  },
+  methods: {
+    /**
+     * 色情報の配列とカード用の配列を初期化
+     */
+    init() {
+      this.colors = CONFIG.COLORS.concat(CONFIG.COLORS);
+      this.colors = _.shuffle(this.colors);
+
+      this.cards.splice(0, this.cards.length);
+      for (let index = 0; index < this.totalCard; index++) {
+        this.cards.push({ text: '?', index: index, status: '' });
+      }
+
+      this.count = CONFIG.COUNT;
+      this.matchCount = 0;
+    },
+
+    /**
+     * 選択されたらカードのテキストを変更
+     */
+    select(index) {
+      const isOpened = this.isOpened(index);
+      if (!isOpened) {
+        this.open(index);
+        this.openCards.push(this.cards[index]);
+      }
+    },
+
+    /**
+     * 選択されたカードが開かれている状態かどうかを返す
+     */
+    isOpened(index) {
+      return (
+        this.cards[index].status === 'open' ||
+        this.cards[index].status === 'match'
+      );
+    },
+
+    /**
+     * カードのデータを更新する
+     */
+    open(index) {
+      this.cards[index].status = 'open';
+      const $card = document.querySelectorAll('li')[index];
+      const colorClass = this.colors[index].class;
+      velocity(
+        $card,
+        {
+          rotateY: ['180deg', '0deg'],
+          tween: 180
+        },
+        {
+          duration: 400,
+          progress: (elements, complete, remaining, start, tweenValue) => {
+            // 半分までアニメーションしたらclassを付与して折り返す
+            if (tweenValue >= 90) {
+              const difference = tweenValue - 90;
+              const rotateY = 90 - difference;
+              $card.style.transform = `rotateY(${rotateY}deg)`;
+              this.updateCardData(index, colorClass);
+            }
+          },
+          complete: () => {
+            this.checkOpenCardLength();
+          }
+        }
+      );
+    },
+
+    /**
+     * カードのデータを更新
+     */
+    updateCardData(index, colorClass) {
+      this.cards[index].status = `${colorClass} open`;
+      this.cards[index].text = this.colors[index].text;
+    },
+
+    /**
+     * 開いているカードの枚数をチェック
+     */
+    checkOpenCardLength() {
+      // 2枚開いていたらカウントを1減らして判定へ
+      if (this.openCards.length === 2) {
+        this.count--;
+        this.judge();
+      }
+    },
+
+    /**
+     * 選択された2枚が同じか判定
+     */
+    judge() {
+      if (this.openCards[0].text === this.openCards[1].text) {
+        this.matchCount += 2;
+        this.openCards.splice(0, 2);
+      } else {
+        this.close();
+      }
+
+      this.checkCount();
+    },
+
+    /**
+     * 選択されたカードを閉じる
+     */
+    close() {
+      const indexs = [
+        { number: this.openCards[0].index },
+        { number: this.openCards[1].index }
+      ];
+
+      this.openCards.splice(0, 2);
+
+      for (let index = 0; index < 2; index++) {
+        const cardIndex = indexs[index].number;
+        const $card = document.querySelectorAll('li')[cardIndex];
+        velocity(
+          $card,
+          {
+            rotateY: ['180deg', '0deg'],
+            tween: 180
+          },
+          {
+            duration: 500,
+            delay: 400,
+            progress: (elements, complete, remaining, start, tweenValue) => {
+              // 半分までアニメーションしたらclassを付与して折り返す
+              if (tweenValue >= 90) {
+                this.cards[cardIndex].status = '';
+                this.cards[cardIndex].text = '?';
+                const difference = tweenValue - 90;
+                const rotateY = 90 - difference;
+                $card.style.transform = `rotateY(${rotateY}deg)`;
+              }
+            }
+          }
+        );
+      }
+    },
+
+    /**
+     * 残り回数をチェック、0なら終了（初期化）
+     */
+    checkCount() {
+      const isAllMatch = this.matchCount === this.totalCard;
+
+      if (isAllMatch) {
+        console.log('clear');
+        this.init();
+      } else if (this.count === 0) {
+        console.log('failed');
+        this.init();
+      }
+    }
+  },
+  created() {
+    this.init();
+  },
+  mounted() {
+    this.$el.style.display = 'block';
   }
 });
